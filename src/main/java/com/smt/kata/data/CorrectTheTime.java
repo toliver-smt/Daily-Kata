d46126b26dab42e5af728b9b8a32842e26b161d9
Etewa <james@siliconmtn.com>
2021-02-25 09:03:18 -0700

    feat(kata): added a time Kata

---
+++ src/main/java/com/smt/kata/data/CorrectTheTime.java
@@ -0,0 +1,36 @@
+package com.smt.kata.data;
+
+/****************************************************************************
+ * <b>Title</b>: CorretTheTime.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Correct the Time.  You have to create a method, 
+ * that corrects a given time string.  There was a problem in addition, 
+ * so many of the time strings are broken.
+ * Time is formatted using the 24-hour clock, so from 00:00:00 to 23:59:59:
+ * Examples
+ * "09:10:01" -> "09:10:01"  
+ * "11:70:10" -> "12:10:10"  
+ * "19:99:99" -> "20:40:39"  
+ * "24:01:01" -> "00:01:01" 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Feb 24, 2021
+ * @updates:
+ ****************************************************************************/
+public class CorrectTheTime {
+
+	/**
+	 * 
+	 */
+	public CorrectTheTime() {
+		super();
+	}
+	
+	public String transform(String time) {
+	
+		return time;
+	}
+}
