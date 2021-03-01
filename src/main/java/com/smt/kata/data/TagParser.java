6cfc73003f3eb73c9493e363db53abc611b50f78
Etewa <james@siliconmtn.com>
2021-02-28 17:35:01 -0700

    feat(tags): added a tag kata to parse html tags

---
+++ src/main/java/com/smt/kata/data/TagParser.java
@@ -0,0 +1,51 @@
+package com.smt.kata.data;
+
+// JDK 11.x
+import java.util.ArrayList;
+import java.util.List;
+
+/****************************************************************************
+ * <b>Title</b>: TagParser.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> In a tag-based language like XML or HTML, contents are 
+ * enclosed between a start tag and an end tag like <tag>contents</tag>. Note 
+ * that the corresponding end tag starts with a /.
+ * 
+ * Given a string of text in a tag-based language, parse this text and retrieve the 
+ * contents enclosed within sequences of well-organized tags meeting the following criterion:
+ * 
+ * The name of the start and end tags must be same. The HTML code <h1>Hello World</h2> 
+ * is not valid, because the text starts with an h1 tag and ends with a non-matching h2 tag.
+ * Tags can be nested as many times as you'd like
+ * For example, in <h1><a>contents</a></h1>, contents is valid.
+ * 
+ * Process each tag and add the valid tags into a collection of tags Tags can consist 
+ * of any printable characters.
+ * 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Feb 27, 2021
+ * @updates:
+ ****************************************************************************/
+public class TagParser {
+
+	/**
+	 * 
+	 */
+	public TagParser() {
+		super();
+	}
+
+	/**
+	 * Parses the content into 
+	 * @param content
+	 * @return
+	 */
+	public List<String> evaluateTags(String tag) {
+		List<String> tags = new ArrayList<>();
+		return tags;
+	}
+}
---
+++ src/test/java/com/smt/kata/data/TagParserTest.java
@@ -0,0 +1,95 @@
+package com.smt.kata.data;
+
+// Junit 5
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+// JDK 11.x
+import java.util.List;
+
+/****************************************************************************
+ * <b>Title</b>: TagParserTest.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Tests the TagParser Kata class
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Feb 27, 2021
+ * @updates:
+ ****************************************************************************/
+class TagParserTest {
+	
+	// Members
+	private TagParser tp = new TagParser();
+
+	/**
+	 * Test method for {@link com.smt.kata.data.TagParser#TagParser()}.
+	 */
+	@Test
+	void testTagParser() throws Exception {
+		TagParser tp = new TagParser();
+		assertNotNull(tp);
+	}
+
+	/**
+	 * Test method for {@link com.smt.kata.data.TagParser#evaluateTags(java.lang.String)}.
+	 */
+	@Test
+	void testEvaluateTagsNull() throws Exception {
+		assertEquals(0, tp.evaluateTags(null).size());
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.data.TagParser#evaluateTags(java.lang.String)}.
+	 */
+	@Test
+	void testEvaluateTagsNoTag() throws Exception {
+		List<String> tags = tp.evaluateTags("This is a test");
+		assertEquals(0, tags.size());
+
+		tags = tp.evaluateTags("<h2 This is a test");
+		assertEquals(0, tags.size());
+
+		tags = tp.evaluateTags("<h2> This is a test");
+		assertEquals(0, tags.size());
+		
+		tags = tp.evaluateTags("<h2 This is a test</h2>");
+		assertEquals(0, tags.size());
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.data.TagParser#evaluateTags(java.lang.String)}.
+	 */
+	@Test
+	void testEvaluateTagsSimples() throws Exception {
+		assertEquals(1, tp.evaluateTags("<h1>Hello World</h1>").size());
+		assertEquals("<h1>Hello World</h1>", tp.evaluateTags("<h1>Hello World</h1>").get(0));
+	}
+
+	/**
+	 * Test method for {@link com.smt.kata.data.TagParser#evaluateTags(java.lang.String)}.
+	 */
+	@Test
+	void testEvaluateTagsDouble() throws Exception {
+		List<String> tags = tp.evaluateTags("<h1>Hello World</h1><div>Another Tag</div>");
+		assertEquals(2, tags.size());
+		assertEquals("<h1>Hello World</h1>", tags.get(0));
+		assertEquals("<div>Another Tag</div>", tags.get(1));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.data.TagParser#evaluateTags(java.lang.String)}.
+	 */
+	@Test
+	void testEvaluateTagsInner() throws Exception {
+		List<String> tags = tp.evaluateTags("<div><span>Another Tag</span><b>Hello World</b></div>");
+		System.out.println(tags);
+		assertEquals(3, tags.size());
+		assertEquals("<div><span>Another Tag</span><b>Hello World</b></div>", tags.get(0));
+		assertEquals("<span>Another Tag</span>", tags.get(1));
+		assertEquals("<b>Hello World</b>", tags.get(2));
+	}
+}
