5522d7cfc85b21179077c77012b9f3792ba536a2
Etewa <james@siliconmtn.com>
2021-03-08 11:18:20 -0700

    docs(doc cleanup): improved the comments

--- src/main/java/com/smt/kata/word/ReverseWord.java
+++ src/main/java/com/smt/kata/word/ReverseWord.java
@@ -17,7 +17,7 @@ package com.smt.kata.word;
  * The input string may contain leading or trailing spaces. However, your reversed 
  * string should not contain leading or trailing spaces. You need to reduce multiple 
  * spaces between two words to a single space in the reversed string. You *** MAY NOT ***
- * utilize String.split() or parsing methods.  You mut loop the phrase and process in a 
+ * utilize String.split() or parsing methods.  You must loop the phrase and process in a 
  * loop.  You may use collections to store words if desired.
  * 
  * <b>Copyright:</b> Copyright (c) 2021
