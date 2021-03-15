d11d56e905ad83e32e7e3e71b25525a8bda87dd2
Etewa <james@siliconmtn.com>
2021-03-15 11:19:45 -0600

    feat(new): added a new roman numeral math kata

---
+++ src/main/java/com/smt/kata/math/RomanNumeralMath.java
@@ -0,0 +1,26 @@
+package com.smt.kata.math;
+
+/****************************************************************************
+ * <b>Title</b>: RomanNumeralMath.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> CHANGE ME!!
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Mar 15, 2021
+ * @updates:
+ ****************************************************************************/
+public class RomanNumeralMath {
+	
+	/**
+	 * Takes two roman numerals and adds them together
+	 * @param first
+	 * @param second
+	 * @return
+	 */
+	public String add(String first, String second) {
+		return first + second;
+	}
+}
