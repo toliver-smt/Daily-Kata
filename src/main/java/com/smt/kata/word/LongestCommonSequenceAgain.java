ea831365e948cb9b0a33e8b0382ea2ee2f65eeb0
smt-etewa <james@siliconmtn.com>
2021-08-12 22:59:17 -0600

    Added the new Longest Common Sequence Kata

---
+++ src/main/java/com/smt/kata/word/LongestCommonSequenceAgain.java
@@ -0,0 +1,38 @@
+package com.smt.kata.word;
+
+/****************************************************************************
+ * <b>Title</b>: LongestCommonSequenceAgain.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Longest Common Sequence Again Kata
+ *  
+ * Write a program that computes the length of the longest common subsequence of 
+ * a variable number of given strings. For example, given "epidemiologist", "refrigeration", 
+ * and "believingmore", it should return "eieio" since that is the longest common subsequence.
+ * 
+ * The sequences are case sensitive.  The sequences do not need to be sequntial,
+ * though they must be in order.
+ * 
+ * Another Example
+ * Given: "bat", "hat", "cat"
+ * Return: "at"
+ * 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Aug 12, 2021
+ * @updates:
+ ****************************************************************************/
+public class LongestCommonSequenceAgain {
+	
+	/**
+	 * Calculates the longest matching sequence of the words
+	 * @param words Words to evaluate
+	 * @return Longest sequence in all words
+	 */
+	public String getSequence(String[] wordsArray) {
+		// Return the longest match
+		return "";
+	}
+}
