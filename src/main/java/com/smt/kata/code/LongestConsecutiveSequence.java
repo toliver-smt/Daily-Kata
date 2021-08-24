a8666adf1c5964e4cd315d9d88b327972df5e7bd
smt-etewa <james@siliconmtn.com>
2021-08-24 13:58:52 -0600

    Added the Longest Consecuitve Sequence Kata

---
+++ src/main/java/com/smt/kata/code/LongestConsecutiveSequence.java
@@ -0,0 +1,40 @@
+package com.smt.kata.code;
+
+/****************************************************************************
+ * <b>Title</b>: LongestConsecutiveSequence.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Longest Consecutive Sequence
+ * 
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * 
+ * You may only use primitives and standard Java.  Collections, Arrays.class, etc .. are not permitted
+ * There should be NO imports in your class
+ * 
+ * Example 1:
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * 
+ * Example 2:
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ * 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Aug 24, 2021
+ * @updates:
+ ****************************************************************************/
+public class LongestConsecutiveSequence {
+	
+	/**
+	 * Finds the longest sequence of numbers on the array
+	 * @param items Items to be calculated against
+	 * @return Largest sequence of numbers in the array
+	 */
+	public int calculate(int[] items) {
+		return items.length;
+	}
+}
