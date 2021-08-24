0405570f8dd866231486c64f4f42e48c0fbe63b4
smt-etewa <james@siliconmtn.com>
2021-08-24 14:01:53 -0600

    Added the Alien Alphabet Kata

---
+++ src/main/java/com/smt/kata/word/AlienAlphabet.java
@@ -0,0 +1,32 @@
+package com.smt.kata.word;
+
+/****************************************************************************
+ * <b>Title</b>: AlienAlphabet.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Alien Alphabet Kata
+ * 
+ * You come across a dictionary of sorted words in a language you've never seen 
+ * before. Write a program that returns the correct order of letters in this language.
+ * 
+ * For example, given ['xww', 'wxyz', 'wxyw', 'ywx', 'ywz'], 
+ * you should return ['x', 'z', 'w', 'y'].
+ * 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Aug 23, 2021
+ * @updates:
+ ****************************************************************************/
+public class AlienAlphabet {
+
+	/**
+	 * Finds the "Alien" alphabet in order
+	 * @param words List of words in the proper order
+	 * @return Order of the characters
+	 */
+	public char[] alphabetize(String[] words) {
+		return new char[0];
+	}
+}
