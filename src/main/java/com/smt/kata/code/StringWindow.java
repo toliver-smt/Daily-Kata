93aafbb40d54ef56e99eb27eb2742590b28f02a6
smt-etewa <james@siliconmtn.com>
2021-11-30 14:02:22 -0700

    Added the String Window Kata

---
+++ src/main/java/com/smt/kata/code/StringWindow.java
@@ -0,0 +1,33 @@
+package com.smt.kata.code;
+
+/****************************************************************************
+ * <b>Title</b>: StringWindow.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> String Window Kata
+ * 
+ * Given a string, find the length of the smallest window that contains every 
+ * distinct character. Characters may appear more than once in the window.
+ * 
+ * For example, given "jiujitsu", you should return 5, corresponding to the final 
+ * five letters.
+ * 
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Nov 30, 2021
+ * @updates:
+ ****************************************************************************/
+public class StringWindow {
+
+	/**
+	 * Find the length of the smallest window
+	 * @param word Word to find the window
+	 * @return Smallest Window.  0 is returned if invalid data.
+	 */
+	public int find(String word) {
+		return word.length();
+	}
+
+}
