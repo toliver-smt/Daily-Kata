248d2691ee2a613403f5c13b3dab172480fc68a7
smt-etewa <james@siliconmtn.com>
2022-02-21 09:26:41 -0700

    Added the Anagram Group Kata

---
+++ src/main/java/com/smt/kata/game/AnagramGroup.java
@@ -0,0 +1,47 @@
+package com.smt.kata.game;
+
+// JDK 11.x
+import java.util.Collection;
+import java.util.List;
+
+/****************************************************************************
+ * <b>Title</b>: AnagramGroup.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Anagram Group Kata
+ * 
+ * Given an array of strings, group anagrams together.
+ * 
+ * For example, given the following array:
+ * 
+ * ['eat', 'ate', 'apt', 'pat', 'tea', 'now']
+ * 
+ * Return:
+ * 
+ * [['eat', 'ate', 'tea'],
+ *  ['apt', 'pat'],
+ *  ['now']]
+ *  
+ *  Constraints
+ *  All words must be lower case or converted to lower case if not.
+ *  Return empty collection if source array is empty or null
+ *  Skip all null words in the array
+ *  
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Feb 21, 2022
+ * @updates:
+ ****************************************************************************/
+public class AnagramGroup {
+
+	/**
+	 * Groups all of the words into anagrams
+	 * @param words Array of words to group
+	 * @return A collection of lists grouping the 
+	 */
+	public Collection<List<String>> findGroups(String[] words) {
+		return null;
+	}
+}
