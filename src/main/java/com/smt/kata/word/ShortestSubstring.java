f191993f644a5140fea638af8872acd27a7e93e1
smt-etewa <james@siliconmtn.com>
2022-03-30 09:30:50 -0600

    Added the shortest substring kata

---
+++ src/main/java/com/smt/kata/word/ShortestSubstring.java
@@ -0,0 +1,39 @@
+package com.smt.kata.word;
+
+/****************************************************************************
+ * <b>Title</b>: ShortestSubstring.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Shortest Substring Kata
+ * 
+ * Given a string and a set of characters, return the shortest substring 
+ * containing all the characters in the set.  The characters in the set can be in 
+ * any order.
+ * 
+ * For example, given the string "figehaeci" and the set of characters {a, e, i}, 
+ * you should return "aeci".
+ * 
+ * Another example, given the string "figehaeci" and the set of characters {i, a, e }, 
+ * you should return "aeci" as well.
+ * 
+ * If there is no substring containing all the characters in the set, return ""
+
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Mar 29, 2022
+ * @updates:
+ ****************************************************************************/
+public class ShortestSubstring {
+
+	/**
+	 * Find the smallest substring for the given word and chatracters
+	 * @param word Word to evaluate against
+	 * @param sequence Characters to find in the smallest substring
+	 * @return Smallest substring.  Empty if not found
+	 */
+	public String find(String word, char[] sequence) {
+		return word;
+	}
+}
