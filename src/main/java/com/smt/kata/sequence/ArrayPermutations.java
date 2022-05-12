7d2643cdedd5d2dc465269360a22d46fe96e1ddc
smt-etewa <james@siliconmtn.com>
2022-05-12 10:17:28 -0600

    Added the array permutations kata

---
+++ src/main/java/com/smt/kata/sequence/ArrayPermutations.java
@@ -0,0 +1,37 @@
+package com.smt.kata.sequence;
+
+// JDK 11.x
+import java.util.ArrayList;
+import java.util.List;
+
+/****************************************************************************
+ * <b>Title</b>: ArrayPermutations.java
+ * <b>Project</b>: SMT-Kata
+ * <b>Description: </b> Array Permutations Kata
+ * 
+ * Given a number in the form of a list of digits, return all possible permutations.
+ *
+ * For example, given [1,2,3], 
+ * return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 
+ * If the array is empty or null, return an empty collection
+ *
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since May 12, 2022
+ * @updates:
+ ****************************************************************************/
+public class ArrayPermutations {
+
+	/**
+	 * Finds all of the possible unique permutations
+	 * @param num Array of ints to permutate
+	 * @return Collection of permutations
+	 */
+	public List<List<Integer>> findPermutations(int[] num) {
+		return new ArrayList<>();
+	}
+}
