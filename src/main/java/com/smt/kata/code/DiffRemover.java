74397d5798909f4c49d9a1778be6dfc9915bc1ab
Billy Larsen <billy@siliconmtn.com>
2022-05-25 10:35:28 -0600

    Create DiffRemover.java
    Adding DiffRemover Kata

---
+++ src/main/java/com/smt/kata/code/DiffRemover.java
@@ -0,0 +1,27 @@
+package com.smt.kata.code;
+
+/**
+ * Given a master int array and removes diff array.  Remove all instances of
+ * elements in remove from master.
+ * 
+ * Example
+ * 
+ * master: {1,2,3,3,4,5,6}, remove: {1,3,5} => {2,4,6}
+ * master: {1,2,3,3,4,5,6}, remove: {} => {1,2,3,3,4,5,6}
+ * master: {1,2,3,3,4,5,6}, remove: {1,2,3,3,4,5,6} => {}
+ * master: {}, remove: {1,3,5} => {}
+ * 
+ * Notes:
+ * 
+ * Order is not guaranteed on incoming lists but return should be ordered low->high
+ * Method always returns an array, even if it is empty or inputs are null.
+ * 
+ * @author raptor
+ *
+ */
+public class DiffRemover {
+
+	public int[] deDiff(int[] master, int[] remove) {
+		return null;
+	}
+}
