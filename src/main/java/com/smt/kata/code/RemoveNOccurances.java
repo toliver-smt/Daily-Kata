900921d8324b87ce5449125f50b4fc89b46f2f91
Billy Larsen <billy@siliconmtn.com>
2022-05-27 10:32:16 -0600

    Create RemoveNOccurances.java

---
+++ src/main/java/com/smt/kata/code/RemoveNOccurances.java
@@ -0,0 +1,26 @@
+package com.smt.kata.code;
+
+/**
+ * Given a list and a number, create a new list that contains each number of list
+ * at most N times, without reordering.  For example if the input number is 2, 
+ * and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next 
+ * [1,2] since this would lead to 1 and 2 being in the result 3 times, and then
+ * take 3, which leads to [1,2,3,1,2,3].  With list [20,37,20,21] and number 1, 
+ * the result would be [20,37,21].
+ * 
+ * Results:
+ * removeOccurances should always return an array, empty if validation fails.
+ * 
+ * validation:
+ * data should not be null
+ * maxOccurances should not be negative.
+ * 
+ * @author raptor
+ *
+ */
+public class RemoveNOccurances {
+
+	public int [] removeOccurances(int [] data, int maxOccurances) {
+    return null;
+	}
+}
