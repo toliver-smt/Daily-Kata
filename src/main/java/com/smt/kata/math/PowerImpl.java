5579b08c403bc2ba83488980687daf261783cde5
William R Larsen Jr <billy@siliconmtn.com>
2022-06-09 12:19:56 -0400

    Added comments.

--- src/main/java/com/smt/kata/math/PowerImpl.java
+++ src/main/java/com/smt/kata/math/PowerImpl.java
@@ -1,5 +1,19 @@
 package com.smt.kata.math;
 
+/**
+ * The purpose of this Kata is to implement the Math.pow(num, pow) functionality yourself.
+ *
+ * The function should take in a double value and proper raise it by the given power.
+ *
+ * You cannot use any Math based libraries to perform the operations.
+ *
+ * See the test class for examples.
+ *
+ * @author raptor
+ * @version 3.0
+ * @since June 9, 2022
+ *
+ */
 public class PowerImpl {
 
 	public double calcPower(double num, int pow) {
