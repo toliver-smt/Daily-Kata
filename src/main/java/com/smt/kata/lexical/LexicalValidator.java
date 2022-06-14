b84b2e3ca77e31e06b62f194cee24667351b5867
William R Larsen Jr <billy@siliconmtn.com>
2022-06-14 12:33:09 -0400

    Added Comments.

--- src/main/java/com/smt/kata/lexical/LexicalValidator.java
+++ src/main/java/com/smt/kata/lexical/LexicalValidator.java
@@ -5,7 +5,27 @@ import com.siliconmtn.data.text.StringUtil;
 /**
  * Part one of a series designed to help design a lexical Analyzer, this Kata focuses on performing string validation before any analysis occurs.
  * 
- * The goal is to write a regex
+ * The goal is to write a regex pattern that will validate an equation 
+ * consists of a left side of numbers and operands (+ or -) 
+ * with any number of spaces between them, an = symbol, 
+ * and a right side consisting of a number.
+ * 
+ * All numbers will be positive.  No letters or other special characters should exist.
+ * 
+ * Example of a valid equation
+ * 
+ * 1+1=2
+ * 1 + 1 = 2
+ *   12 + 12 + 4 - 5 - 2=34
+ *   
+ *Example of invalid
+ *1 = 2
+ * = 2
+ *1
+ *1 + a = 2
+ *1 + 2
+ *1 + 2 2 = 7
+ *1 * 5 = 5 
  * @author raptor
  *
  */
